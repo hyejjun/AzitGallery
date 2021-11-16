c2d17a1f110f7b6f345ac9ab0bc8728d3d30a335
hyejjun <hjlee9725@gmail.com>
2021-11-16 12:03:14 +0900

    auction view 사진 연결

--- back/routers/view/view.controller.js
+++ back/routers/view/view.controller.js
@@ -98,6 +98,18 @@ let get_auction_view = async (req, res) => {
         // @ 이 부분을 따로 websocket으로 받을 수 있어야할듯..
         let left_time = timeForToday(end_date)
 
+
+
+        let result5 = await ItemImg.findAll({where:{item_id:idx}})
+
+        let pic_array = [...result5]
+        let item_img_link = []
+
+        pic_array.forEach((v,k)=>{
+            item_img_link.push(v.dataValues.item_img_link)
+        })
+
+
         data = {
             result_msg: 'OK',
             nick_name,
@@ -107,7 +119,8 @@ let get_auction_view = async (req, res) => {
             color,
             bid_price,
             currency,
-            left_time
+            left_time,
+            item_img_link
         }
 
     } catch (error) {
